72524507bad958e36b50999f53e8a004c1c994c2
TomL384 <350286454@tdsb.ca>
2026-03-24 11:13:59 -0400

    Add methods to check even/odd and teacher/student

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,25 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		public String evenOrOdd (int int1){
+			if (int1 % 2 == 0){
+			return "Even"}
+		return "Odd"}
+
+	public String teacherOrStudent (String v1){
+		if (v1.equalsTo("kalisz")){
+			return "Teacher"}
+		return "Student"}
+
+	public int fatherFromZero (int int1){
+		if (int1 > 0){
+			int1= int1 + 5
+			return int1}
+		if (int1 < 0){
+			int1 = int1 - 5
+			return int1}
+		return int1}
+		
 
 	}
 
