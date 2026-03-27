e5f6b94a97d4608eb095e825b3b94a1cd7a17f4c
TomL384 <350286454@tdsb.ca>
2026-03-27 15:25:41 +0000

    Final

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,26 +1,32 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-		public String evenOrOdd (int int1){
-			if (int1 % 2 == 0){
-			return "Even"}
-		return "Odd"}
+	public static void main(String args[]) { }
 
-	public String teacherOrStudent (String v1){
-		if (v1.equalsTo("kalisz")){
-			return "Teacher"}
-		return "Student"}
+	public static String evenOrOdd (int int1)  {  
+		if (int1 % 2 == 0){
+			return "Even" ;
+		}
+		return "Odd";
+	}
 
-	public int fatherFromZero (int int1){
+	public static String teacherOrStudent (String v1){
+		if (v1.equals("Kalisz")){
+			return "Teacher";
+		}
+		return "Student";
+	}
+
+	public static int fartherFromZero (int int1){
 		if (int1 > 0){
-			int1= int1 + 5
-			return int1}
+			int1= int1 + 5 ;
+		}
 		if (int1 < 0){
-			int1 = int1 - 5
-			return int1}
-		return int1}
+			int1 = int1 - 5 ;
+		}  
+		return int1 ; 
+		 }
 		
 
-	}
+	
 
 }
